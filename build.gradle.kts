7a1a72d71c60c8c39b4f95169aba740244251f61
ni-xon <nixon.s.uch@gmail.com>
2023-08-19 23:13:39 +1000

    init: bootstrap kotlin project with aws dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "com.nixonsu"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
+    implementation("com.amazonaws:aws-lambda-java-events:3.11.1")
+    runtimeOnly("com.amazonaws:aws-lambda-java-log4j2:1.5.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
+
+application {
+    mainClass.set("MainKt")
+}
