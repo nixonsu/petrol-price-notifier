7b3555d4bad46801230bf725a7513c927a885a96
ni-xon <nixon.s.uch@gmail.com>
2023-08-20 16:27:29 +1000

    feat: add base cloudformation template

--- src/main/kotlin/com/nixonsu/Main.kt
+++
@@ -1,9 +0,0 @@
-package com.nixonsu
-
-import com.nixonsu.service.PetrolPriceService
-
-fun main(args: Array<String>) {
-    val petrolPriceService = PetrolPriceService()
-
-    println(petrolPriceService.getLowestU91PriceInAustralia())
-}
