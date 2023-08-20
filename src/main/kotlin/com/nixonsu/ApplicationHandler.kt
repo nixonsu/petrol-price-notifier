2348114a863787f779e420fc180f9c56bcc55f75
ni-xon <nixon.s.uch@gmail.com>
2023-08-20 16:27:53 +1000

    feat: add rough sns publish logic

--- src/main/kotlin/com/nixonsu/ApplicationHandler.kt
+++ src/main/kotlin/com/nixonsu/ApplicationHandler.kt
@@ -1,8 +1,12 @@
 package com.nixonsu
 
+import com.amazonaws.regions.Regions
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.events.CloudWatchLogsEvent
+import com.amazonaws.services.sns.AmazonSNS
+import com.amazonaws.services.sns.AmazonSNSClientBuilder
+import com.amazonaws.services.sns.model.PublishRequest
 import com.nixonsu.service.PetrolPriceService
 
 class ApplicationHandler : RequestHandler<CloudWatchLogsEvent, String> {
@@ -20,8 +24,21 @@ class ApplicationHandler : RequestHandler<CloudWatchLogsEvent, String> {
 
         logger.log("Publishing to SNS...")
 
-        logger.log("Done!")
+        // Initialize the SNS Client
+        val snsClient: AmazonSNS = AmazonSNSClientBuilder.standard()
+            .withRegion(Regions.AP_SOUTHEAST_2)
+            .build()
 
-        return "Finished"
+        // Define SNS Topic ARN
+        val snsTopicArn = "xxx"
+
+        // Create Publish Request
+        val publishRequest = PublishRequest(snsTopicArn, "CloudWatch Event Triggered!", "CloudWatch Event")
+
+        // Publish the message
+        val publishResponse = snsClient.publish(publishRequest)
+        logger.log("Message published, message ID: ${publishResponse.messageId}")
+
+        return "Done"
     }
 }
