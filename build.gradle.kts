c803703275dcd34a23a131482da883fee1961cb3
ni-xon <nixon.s.uch@gmail.com>
2023-08-20 16:27:40 +1000

    build: add aws sns dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ repositories {
 dependencies {
     implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
     implementation("com.amazonaws:aws-lambda-java-events:3.11.1")
+    implementation("com.amazonaws:aws-java-sdk-sns:1.12.1")
     runtimeOnly("com.amazonaws:aws-lambda-java-log4j2:1.5.1")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.12.3")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.3")
@@ -26,8 +27,4 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
-}
-
-application {
-    mainClass.set("com.nixonsu.MainKt")
 }
