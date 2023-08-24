3f38b4f70a085c064f49f78c2e4e54d423dfb916
ni-xon <nixon.s.uch@gmail.com>
2023-08-24 19:33:04 +1000

    fix: temporarily disable exit on error in script

--- ops/scripts/create-s3.sh
+++ ops/scripts/create-s3.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Temporarily disable exit on error
+set +e
+
 # Check if bucket to store project jar exists
 aws s3api head-bucket --bucket petrol-price-notifier 2>/dev/null
 
-# Exit code of last command is stored in $? variable
-# If it's 0, the bucket exists; otherwise, it does not, or inadequate permission to access it
-if [ $? -eq 0 ]; then
+# Capture exit code of the last command
+exit_code=$?
+
+# Re-enable exit on error
+set -e
+
+# If exit code is 0, the bucket exists; otherwise, it does not, or inadequate permission to access it
+if [ $exit_code -eq 0 ]; then
   echo "Bucket exists, skipping creating bucket"
 else
   echo "Bucket does not exist, creating bucket"
@@ -14,5 +22,5 @@ else
   # Create S3 bucket to store project jar
   aws s3api create-bucket \
   --bucket petrol-price-notifier \
-  --create-bucket-configuration LocationConstraint=ap-southeast-2
+  --create-bucket-configuration LocationConstraint=${AWS_DEFAULT_REGION}
 fi
