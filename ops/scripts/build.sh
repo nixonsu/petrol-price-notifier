c9024c347af01e41f6b6c351326a2caaa8864460
ni-xon <nixon.s.uch@gmail.com>
2023-08-24 19:42:10 +1000

    fix: add mandatory field to aws package step

--- ops/scripts/build.sh
+++ ops/scripts/build.sh
@@ -10,4 +10,5 @@ aws s3 cp ./build/libs/petrol-price-notifier.jar s3://petrol-price-notifier/petr
 # Package template.yaml -> packaged-template.yaml
 aws cloudformation package \
 --template-file ./ops/deployment/template.yaml \
+--s3-bucket petrol-price-notifier \
 --output-template-file ./ops/deployment/packaged-template.yaml
