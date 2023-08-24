534ab46ac5cd87acaffea613622494879a94ab1c
ni-xon <nixon.s.uch@gmail.com>
2023-08-24 19:17:31 +1000

    cicd: decompose scripts and add setup step to pipeline

--- ops/scripts/build.sh
+++ ops/scripts/build.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 # Build fat jar
 ./gradlew shadowJar
 
-# Copy fat jar to S3
+# Upload fat jar to S3
 aws s3 cp ./build/libs/petrol-price-notifier.jar s3://petrol-price-notifier/petrol-price-notifier-${GITHUB_SHA:0:8}.jar
 
-# Package template.yaml -> packaged-template.yaml with paramters populated e.g. jar name
+# Package template.yaml -> packaged-template.yaml
 aws cloudformation package \
 --template-file ./ops/deployment/template.yaml \
 --output-template-file ./ops/deployment/packaged-template.yaml
---
+++ ops/scripts/create-s3.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Check if bucket to store project jar exists
+aws s3api head-bucket --bucket petrol-price-notifier 2>/dev/null
+
+# Exit code of last command is stored in $? variable
+# If it's 0, the bucket exists; otherwise, it does not, or inadequate permission to access it
+if [ $? -eq 0 ]; then
+  echo "Bucket exists, skipping creating bucket"
+else
+  echo "Bucket does not exist, creating bucket"
+
+  # Create S3 bucket to store project jar
+  aws s3api create-bucket \
+  --bucket petrol-price-notifier \
+  --create-bucket-configuration LocationConstraint=${AWS_DEFAULT_REGION}
+fi
