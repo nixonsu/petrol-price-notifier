72ee2397b856ef5689f352547ddcf4e8589f0cf4
ni-xon <nixon.s.uch@gmail.com>
2023-08-27 12:08:05 +1000

    debug: try set user-agent

--- src/main/kotlin/com/nixonsu/clients/PetrolSpyClient.kt
+++ src/main/kotlin/com/nixonsu/clients/PetrolSpyClient.kt
@@ -45,6 +45,7 @@ class PetrolSpyClient(private val httpClient: HttpClient) {
         return HttpRequest.newBuilder()
             .GET()
             .uri(URI.create(PETROL_SPY_SPECIFIC_STATION_URL))
+            .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537")
             .build()
     }
 
