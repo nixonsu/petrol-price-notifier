968ba50fb79b2e4d4583c17cdc08a90965ca1445
ni-xon <nixon.s.uch@gmail.com>
2023-09-12 22:57:01 +1000

    build: update serialisation module and fix gradle warning

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 dependencies {
     implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
     implementation("com.amazonaws:aws-java-sdk-sns:1.12.1")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.7.1")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.3")
     implementation("org.jsoup:jsoup:1.16.1")
     implementation("org.slf4j:slf4j-api:1.7.30")
@@ -26,16 +26,17 @@ dependencies {
     testImplementation("io.mockk:mockk:1.13.7")
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 tasks.test {
     useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
-}
-
-application {
-    mainClass.set("com.nixonsu.RuntimeHandlerKt")
 }
 
 tasks {
